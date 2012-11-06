a15f770a23fa56064f1a86e76b669dec1b5c99a3
Nick Funnell <nick@nick.gs>
2012-11-05 17:47:25 -0800

    updated reverse string

--- reverse-string.cpp
+++ reverse-string.cpp
@@ -10,18 +10,38 @@ char* reverse( const char *str ) {
   char *newstr = new char[ len+1 ];
   char temp;
   for( int i=0; i<len+1; i++ ) {
-    newstr[i] = str[len-i];
+    newstr[i] = str[len-i];   
   }
   return newstr;
 }
+
+void reverse_words_helper( char* str ) {
+  if( str == NULL ) {
+    return;
+  }
+  reverse_words_helper( strtok( NULL, " "));
+  cout << str << " ";
+}
+
+void reverse_words( const char *str ) {
+  int len = strlen( str );
+  char* newstr = new char [ len+1 ];
+  strcpy( newstr, str );
+  reverse_words_helper( strtok( newstr, " "));
+}
+
+
 
 int main( int argc, char **argv )
 {
   char *s1 = "Hello world!";
   char *s2 = "A man, a plan, a canal, Panama";
+  char *x;
   cout << "1: " << s1 << endl << "2: " << s2 << endl << endl;
-  s1 = reverse(s1);
-  s2 = reverse(s2);
+  x  = reverse(s1);
+  x = reverse(s2);
   cout << "1: " << s1 << endl << "2: " << s2 << endl << endl;
+  reverse_words(s1);
+  reverse_words(s2);
   return 0;
 }
