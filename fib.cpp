b70c40b8d666287987c99d3c00ac06f978f9f943
Nick Funnell <nick@nick.gs>
2012-12-13 19:30:37 -0800

    ran astyle on everything

--- fib.cpp
+++ fib.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int fib( int n ) {
-  if( n == 0 || n == 1 ) {
-    return n;
-  }
-  return fib(n-2) + fib(n-1);
+    if( n == 0 || n == 1 ) {
+        return n;
+    }
+    return fib(n-2) + fib(n-1);
 }
 
 int main( int argc, char **argv ) {
-  for( int i=0; i<10; i++ ) {
-    cout << i << " " << fib(i) << endl;
-  }
-  return 0;
+    for( int i=0; i<10; i++ ) {
+        cout << i << " " << fib(i) << endl;
+    }
+    return 0;
 }
--- mtable.cpp
+++ mtable.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main( int argc, char **argv ) {
-  for( int row=1; row<=12; row++ ) {
-    for( int col=1; col<=12; col++ ) {
-      cout << setw(4)  << row * col;
+    for( int row=1; row<=12; row++ ) {
+        for( int col=1; col<=12; col++ ) {
+            cout << setw(4)  << row * col;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
+    return 0;
 }
