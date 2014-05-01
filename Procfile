bb130415ffb8c6976167ede6e86345dc13aad935
Nicholas Funnell <nick@nick.gs>
2014-05-01 01:41:06 -0400

    can send many messages from on producer and RMQ will split it to many consumer instances

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 eat: python consume.py
-con: python consume.py
-out: python consume.py
--- prod.py
+++ prod.py
@@ -7,9 +7,18 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print " [x] Sent 'Hello World!'"
+def send():
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    print " [x] Sent 'Hello World!'"
+
+x=10
+while x > 0:
+    send()
+    x = x-1
+
 connection.close()
+
+print " [x] Done sending!"
 
