02383f916d316bceb63c98e7e60ef7520fb94d6a
Nick Fun <nick@nick.gs>
2014-05-15 17:29:03 -0700

    plaing with groovy agian

---
+++ closures.groovy
@@ -0,0 +1,21 @@
+// Groovy is fun
+name = "Nick"
+msg = "Hello, ${name}"
+
+fnGreet = { println msg } // closure!
+fnGreet();
+
+fnDouble = { it * 2 }
+start = [ 10, 100, 1000 ]
+finish = start.collect(fnDouble)
+
+println "START:"
+for (i in start) {
+  println i
+}
+
+println "FINISH:"
+for (i in finish) {
+  println i
+}
+
