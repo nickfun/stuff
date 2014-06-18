e5b5def3b0b785ca70900b3a9bf28c1aa21c1cd3
Nick Fun <nick@nick.gs>
2014-06-18 12:05:52 -0700

    java can be groovy

---
+++ rock.groovy
@@ -0,0 +1,58 @@
+import java.util.Random;
+
+public class RPS {
+
+    public static String getComputerChoice() {
+	Random rng = new Random( System.nanoTime() );
+	double choice = rng.nextDouble();
+	if( choice <= 0.33 ) {
+	    return "rock";
+	}
+	if( choice <= 0.66 ) {
+	    return "paper";
+	}
+	return "scissors";
+    }
+
+    public static void sayWinner(String choice1, String choice2 ) {
+	System.out.print(choice1 + " VS " + choice2 + " -- ");
+	if( choice1.equals(choice2) ) {
+	    // TIE
+	    System.out.println("It's a tie!");
+	    return;
+	}
+	if( choice1.equals("rock") ) {
+	    if( choice2.equals("paper") ) {
+		System.out.println("Paper covers rock");
+	    } else {
+		System.out.println("Rock breaks scissors");
+	    }
+	} else if( choice1.equals("paper") ) {
+	    if( choice2.equals("scissors") ) {
+		System.out.println("Scissors cuts paper");
+	    } else {
+		System.out.println("Paper covers rock");
+	    }
+	} else if( choice1.equals("scissors") ) {
+	    if( choice2.equals("paper") ) {
+		System.out.println("Scissors cuts paper");
+	    } else {
+		System.out.println("Rock breaks scissors");
+	    }
+	} else {
+	    System.out.println("???");
+	}
+    }    
+
+    public static void main( String[] args ) {
+        int limit = 10;
+        int i=0;
+        if (args.length >= 1) {
+            limit = new Integer(args[0]);
+        }
+        System.out.println("Limit is " + limit);
+	for( i=0; i<limit; i++ ) {
+            sayWinner( getComputerChoice(), getComputerChoice() );
+	}
+    }
+}
