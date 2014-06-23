2f3ccd717e5cbf9af9c6291b326ee15653275414
Nick Funnell <nick@nick.gs>
2014-06-23 06:07:33 +0000

    input file for testing

--- jyaml/src/main/java/gs/nick/MyMain.java
+++ jyaml/src/main/java/gs/nick/MyMain.java
@@ -1,8 +1,17 @@
 package gs.nick;
 
+import org.yaml.snakeyaml.Yaml;
+
 public class MyMain {
     public static void main( String[] args) {
 	System.out.println("Hello");
 	System.out.println("World");
+
+	test();
+    }
+
+    public static void test() {
+	System.out.println("I am test");
     }
 }
+
