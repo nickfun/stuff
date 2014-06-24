09cecff29015171812d9366e57fc6d17c4a5f54c
Nick Funnell <nick@nick.gs>
2014-06-24 05:48:54 +0000

    Learning to deconstruct the Yaml data. The docs online for SnakeYaml could be better.

--- jyaml/run.sh
+++ jyaml/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "=== compile"
+mvn package
+clear
 echo "=== begin ==="
 java -cp target/classes:target/dependency/* gs.nick.MyMain
 echo "=== done ==="
--- jyaml/src/main/java/gs/nick/MyMain.java
+++ jyaml/src/main/java/gs/nick/MyMain.java
@@ -2,6 +2,8 @@ package gs.nick;
 
 import org.yaml.snakeyaml.Yaml;
 import java.io.*;
+import java.util.Map;
+import java.util.List;
 
 public class MyMain {
     public static void main( String[] args) throws FileNotFoundException {
@@ -15,8 +17,13 @@ public class MyMain {
 	System.out.println("I am test");
 	InputStream is = new FileInputStream(new File("input.yaml"));
 	Yaml y = new Yaml();
-	Object data = y.load(is);
-	System.out.println(data);
+	Map<String, Object> data = (Map<String, Object>)  y.load(is);
+	Map<String, Object> config = (Map<String, Object>) data.get("config");
+        List<Object> packets = (List<Object>) data.get("packets");
+        System.out.println("config is");
+        System.out.println(config);
+        System.out.println("Packets is");
+        System.out.println(packets);
     }
 }
 
