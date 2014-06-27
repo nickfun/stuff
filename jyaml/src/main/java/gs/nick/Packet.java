5393b38857bbedc1176872e8fa06751397294b20
Nick Funnell <nick@nick.gs>
2014-06-27 03:58:49 +0000

    Some output to verify that I am getting what I expect. I should just setup jUnit...

--- jyaml/src/main/java/gs/nick/Packet.java
+++ jyaml/src/main/java/gs/nick/Packet.java
@@ -4,11 +4,26 @@ import java.util.Date;
 
 public class Packet {
 
-	public int typeid;
-	public int objectid;
-	public int asn1;
-	public Date date;
-	public float lat;
-	public float lng;
-
+    public int typeid;
+    public int objectid;
+    public int asn1;
+    public Date date;
+    public float lat;
+    public float lng;
+    public String comment;
+    
+    public String toString() {
+        String str;
+        if (date == null) {
+            str = "-null-";
+        } else {
+            str = date.toString();
+        }
+        if (comment == null) {
+            comment = "-null-";
+        }
+        return String.format("typeid: %d\nobject: %d\nasn1: %d\ndate: %s\nlat: %f\nlng: %f\ncomment: %s\n",
+                             typeid, objectid, asn1, str, lat, lng, comment);
+    }
+    
 }
