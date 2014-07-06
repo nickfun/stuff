cc1f8b36ff9871faff52d87d414be2061c3742b2
Nick Funnell <nick@nick.gs>
2014-07-05 23:30:37 -0700

    trying to remember recursive-style loops

--- elisp/data.el
+++ elisp/data.el
@@ -6,3 +6,12 @@
 (car data)
 (cdr data)
 
+(setq data1 '("amy" "ben" "chad" "david" "emily" "frank" "gene" "hector"))
+
+(defun printrr (x)
+  (if (not x) (print "done")
+    (progn
+      (print (car x))
+      (printrr (cdr x)))))
+
+(printrr data1)
