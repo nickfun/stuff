26519ed9a47590036652e3e1989487b1f09069ea
Nick Funnell <nick@nick.gs>
2014-08-06 06:17:40 +0000

    aphyr tutorial

--- clj/src/NFClojure/core.clj
+++ clj/src/NFClojure/core.clj
@@ -3,3 +3,11 @@
 (defn whoami [] "I am Nick F")
 
 (defn -main [] (println (whoami)))
+
+(defn inc-more [nums]
+  (if (first nums)
+    ; nums is a list
+    (cons (inc (first nums))
+          (inc-more (rest nums)))
+    ; ELSE: nums is empty, return empty list
+    (list)))
