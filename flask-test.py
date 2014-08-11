9d4aff6e1c2019582872bfffad15b6311c0a3eb9
Nick Fun <nick@nick.gs>
2014-08-11 16:10:14 -0700

    flask

--- flask-test.py
+++ flask-test.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+#
+# General Functions
+#
+
+def getData():
+    data = {
+        "frank@gmail.com": "123-123-123",
+        "sam@yahoo.com": "321-321-321",
+        "hello@runbox.com": "753-753-753",
+    }
+    return data
+
+#
+# Routes
+#
 @app.route("/")
 def index():
     str = ""
@@ -19,6 +34,19 @@ def hello():
 def msg(s):
     return "Your message is <b>%s</b><br>I hope you enjoyed the message!" % s
 
+@app.route("/mail/<key>")
+def webProxyData(key):
+    mydata = getData();
+    return "<b>%s</b> : %s" % (key, mydata[key])
+
+@app.route("/mail")
+def webProxyDataKeys():
+    mydata = getData()
+    return keys(myData)
+
+#
+# Execute
+#
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
