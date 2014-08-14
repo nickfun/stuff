de01f46702f9f5cd12d63581017974307e0defcc
Nick Funnell <nick@nick.gs>
2014-08-14 02:23:57 +0000

    Callables sleep for same amount of time

--- tpool/BigJob.java
+++ tpool/BigJob.java
@@ -5,6 +5,7 @@ public class BigJob implements Callable<String> {
 
     public String call() throws Exception {
         int n = ThreadLocalRandom.current().nextInt(500);
+        n = 500;
         Thread.sleep(500+n);
         System.out.print("**");
         return "" + (500+n);
