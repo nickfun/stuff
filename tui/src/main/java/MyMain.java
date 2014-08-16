ed6b99c4453cca680d5568af24763607822cbb28
Nick Fun <nick@nick.gs>
2014-08-15 17:23:11 -0700

    cant get color package to resolve

--- tui/src/main/java/MyMain.java
+++ tui/src/main/java/MyMain.java
@@ -3,6 +3,9 @@ package gs.nick;
 import java.nio.charset.Charset;
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.screen.*;
+//import com.googlecode.lanterna.terminal.Color;
+
 
 class MyMain {
 
@@ -15,10 +18,12 @@ class MyMain {
 
     public void execute() throws Exception {
 
-        Terminal term = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
-        term.enterPrivateMode();
-        System.out.println("hello");
+        Screen screen = TerminalFacade.createScreen();
+        screen.startScreen();
+        // draw something ???
+        screen.putString(5,5,"Hello world!", Terminal.Color.Red, Terminal.Color.Green, false, false, false);
+        screen.refresh();
         Thread.sleep(3000);
-        term.exitPrivateMode();
+        screen.stopScreen();
     }
 }
