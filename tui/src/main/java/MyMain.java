80c7ecfe5b43b0cf310dba241ce1ab98edc6507c
Nick Fun <nick@nick.gs>
2014-08-15 17:17:20 -0700

    builds in maven, runs Terminal layer

--- tui/src/main/java/MyMain.java
+++ tui/src/main/java/MyMain.java
@@ -1,6 +1,24 @@
+package gs.nick;
+
+import java.nio.charset.Charset;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.terminal.*;
+
 class MyMain {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting!");
+
+        MyMain app = new MyMain();
+        app.execute();
+    }
+
+    public void execute() throws Exception {
+
+        Terminal term = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        term.enterPrivateMode();
+        System.out.println("hello");
+        Thread.sleep(3000);
+        term.exitPrivateMode();
     }
 }
