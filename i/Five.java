48da3219038d873edb479a6960a2c2e382d2cad8
Nick F <nick@nick.gs>
2014-08-29 23:06:15 -0700

    begin 1.5

---
+++ i/Five.java
@@ -0,0 +1,15 @@
+/**
+ * Nick Fun
+ * 2014-08-29
+ * CTCI 1.5
+ * basic string comparison with counts of repeate characters. input: aaaaabbbc output: a5b3c1
+ */
+
+class Five {
+
+      public static void main(String[] args) {
+	  System.out.println("Hello World!");
+      }
+    
+}
+
