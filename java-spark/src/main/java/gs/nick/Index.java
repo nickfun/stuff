5b8bfc1a596377e6cf58f1e1c4528b7125f16699
Nick Fun <nick@nick.gs>
2014-09-03 12:51:16 -0700

    basic spark builds and runs hello world

--- java-spark/src/main/java/gs/nick/Index.java
+++ java-spark/src/main/java/gs/nick/Index.java
@@ -1,3 +1,5 @@
+package gs.nick;
+
 import static spark.Spark.*;
 
 import spark.*;
@@ -5,9 +7,11 @@ import spark.*;
 public class Index {
 
     public static void main(String[] args) {
+        System.out.println("Define: GET /");
         get( new Route("/") {
                 @Override
                 public Object handle(Request req, Response res) {
+                    System.out.println("A request to / is happening");
                     return "Hello, world!";
                 }
             }
