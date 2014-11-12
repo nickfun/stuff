bc6d787445ef194414e2afb71057fcfb6b09dc7a
Nick Fun <nick@nick.gs>
2014-11-12 11:14:24 -0800

    Playing with java - a public method can not be overridden and then declared private

---
+++ inheirt/MyChild.java
@@ -0,0 +1,12 @@
+public class MyChild extends MyParent {
+
+    private int num;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int n) {
+        num = n;
+    }
+}
