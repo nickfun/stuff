91ee601e482822acb06648c7beb0c5dda01e0b2b
Nick Funnell <nick@nick.gs>
2015-06-27 01:52:31 +0000

    c and racket/sicp

---
+++ int-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+  int x = 12;
+  sprintf("x is %d\n", x);
+
+  return 0;
+}
---
+++ scheme/test.rkt
@@ -0,0 +1,6 @@
+#lang planet neil/sicp
+
+(define (abs x)
+        (cond ((> x 0) x)
+              ((< x 0) (- x))
+              ((= x 0) x)))
