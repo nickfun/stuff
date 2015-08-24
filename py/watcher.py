fc1ce5eb0b8dac2a7ba5e292b4ee6159fb3e6ea1
Nicholas Funnell <nick@nick.gs>
2015-08-24 08:11:36 -0700

    got it to watch and respond to files of my extension be modified

--- py/watcher.py
+++ py/watcher.py
@@ -7,46 +7,53 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
-def bruno():
-    # http://brunorocha.org/python/watching-a-directory-for-file-changes-with-python.html
-    somehow_invoke_MyHandler_from_below()
-
- class MyHandler(PatternMatchingEventHandler):
+class MyHandler(PatternMatchingEventHandler):
     patterns = ["*.duplo.json"]
 
+#    def __init__(self, name):
+#        self.name = name
+#        print "name is {}".format(name)
+#
     def process(self, event):
-        """
-        event.event_type 
-            'modified' | 'created' | 'moved' | 'deleted'
-        event.is_directory
-            True | False
-        event.src_path
-            path/to/observed/file
-        """
-        # the file will be processed there
-        print event.src_path, event.event_type  # print now only for debug
+        # print now only for debug
+        print "{}: {}".format(event.event_type, event.src_path)
 
     def on_modified(self, event):
         self.process(event)
 
-# ==========================
-# ==========================
-# ==========================
+def bruno():
+    # http://brunorocha.org/python/watching-a-directory-for-file-changes-with-python.html
+    path = "."
+    event_handler = MyHandler("nick")
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+    print "\nDone?"
+    
 
 def example():
-    if __name__ == "__main__":
-        logging.basicConfig(level=logging.INFO,
-                            format='%(asctime)s - %(message)s',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        path = sys.argv[1] if len(sys.argv) > 1 else '.'
-        event_handler = LoggingEventHandler()
-        observer = Observer()
-        observer.schedule(event_handler, path, recursive=True)
-        observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == '__main__':
+    bruno()
